7cbda898d40b44ef981cee3ecfb72876e4de95c0
marwe <marwe@DESKTOP-H3KS6MS>
2017-12-17 19:56:09 +0100

    Premier projet Spring
    Signed-off-by: marwe <marwe@DESKTOP-H3KS6MS>

---
+++ BanqueSI_Spring/src/main/java/org/glsid/BanqueSiSpringApplication.java
@@ -0,0 +1,12 @@
+package org.glsid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanqueSiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanqueSiSpringApplication.class, args);
+	}
+}
