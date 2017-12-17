599d474444a5d4340f924e6eb47e3925ce61ebb4
marwe <marwe@DESKTOP-H3KS6MS>
2017-12-17 21:08:44 +0100

    Service Métier

--- BanqueSI_Spring/src/main/java/org/glsid/metier/ClientMetier.java
+++ BanqueSI_Spring/src/main/java/org/glsid/metier/ClientMetier.java
@@ -1,5 +1,12 @@
 package org.glsid.metier;
 
+import java.util.List;
+
+import org.glsid.entities.Client;
+
 public interface ClientMetier {
 
+	public Client saveClient(Client client);
+	
+	public List<Client> listClient();
 }
---
+++ BanqueSI_Spring/src/main/java/org/glsid/metier/ClientMetierImpl.java
@@ -0,0 +1,34 @@
+package org.glsid.metier;
+
+import java.util.List;
+
+import org.glsid.dao.ClientRepository;
+import org.glsid.entities.Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+//Pour considerer que cette classe comme un bean Spring
+//Générélement pour les objets de la couche métier
+//Injection des dépendances via XML ou annotations
+//On peut utiliser aussi @Component
+//Si on ne fait pas @Service ou @Component , @Autowired ne vas pas marcher
+@Service
+public class ClientMetierImpl implements ClientMetier{
+	//Autowired pour implementer automatiquement l'interface Repository
+	//Spring injecter une implementation 
+	@Autowired 
+	private ClientRepository clientRepository;
+	
+	@Override
+	public Client saveClient(Client client) {
+		// TODO Auto-generated method stub
+		return clientRepository.save(client);
+	}
+
+	@Override
+	public List<Client> listClient() {
+		// TODO Auto-generated method stub
+		return clientRepository.findAll();
+	}
+
+}
