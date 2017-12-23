085aac108fd62e13a49f97fde3bf974aaa7fe491
marwe <marwe@DESKTOP-H3KS6MS>
2017-12-23 23:45:56 +0100

    explication

--- BanqueSI_Spring/src/main/java/org/glsid/dao/OperationRepository.java
+++ BanqueSI_Spring/src/main/java/org/glsid/dao/OperationRepository.java
@@ -3,7 +3,9 @@ package org.glsid.dao;
 import org.glsid.entities.Operation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//extends JPA<Entities,Id> : Injection de toutes les opérations de bases
+//extends JPA<Entities,Id> : Spring va créer une implémentation dans laquelle 
+// il va injecter toutes les opérations de bases
+
 public interface OperationRepository extends JpaRepository<Operation, Long>{
 
 	
