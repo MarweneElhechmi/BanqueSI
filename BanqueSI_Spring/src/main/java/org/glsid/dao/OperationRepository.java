0d632103ff3fc8435a7d6273d3aa5285542da75c
marwe <marwe@DESKTOP-H3KS6MS>
2017-12-23 16:32:56 +0100

    Explication de JPA<Entities,Id>

--- BanqueSI_Spring/src/main/java/org/glsid/dao/OperationRepository.java
+++ BanqueSI_Spring/src/main/java/org/glsid/dao/OperationRepository.java
@@ -4,7 +4,6 @@ import org.glsid.entities.Operation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 //extends JPA<Entities,Id> : Injection de toutes les opérations de bases
-
 public interface OperationRepository extends JpaRepository<Operation, Long>{
 
 	
