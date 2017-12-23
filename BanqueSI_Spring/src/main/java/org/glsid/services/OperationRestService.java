9e4be9408ef75ef58c926b8e8b0b5ffdfdb41016
marwe <marwe@DESKTOP-H3KS6MS>
2017-12-23 14:28:52 +0100

    Le service qu'on va utiliser pour le test des opérations

---
+++ BanqueSI_Spring/src/main/java/org/glsid/services/OperationRestService.java
@@ -0,0 +1,46 @@
+package org.glsid.services;
+
+import org.glsid.metier.OperationMetier;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OperationRestService {
+
+	@Autowired
+	private OperationMetier operationsMetier ;
+
+	// On a utiliser PUT , parce que le versement c'est une mise à jour du solde.
+	// Ici on a utiliser @RequestParam , parce que ce sont pas des données JSON
+	// On va les envoyés dans le corps de la requêtte 
+	
+	@RequestMapping(value="/versement",method=RequestMethod.PUT)
+	public boolean verser(
+			 @RequestParam String codeCompte
+			,@RequestParam double montant
+			,@RequestParam Long codeEmploye) {
+		return operationsMetier.verser(codeCompte, montant, codeEmploye);
+	}
+
+	@RequestMapping(value="/retrait",method=RequestMethod.PUT)
+	public boolean retirer(@RequestParam String codeCompte
+			,@RequestParam double montant
+			,@RequestParam Long codeEmploye) {
+		return operationsMetier.retirer(codeCompte, montant, codeEmploye);
+	}
+
+	@RequestMapping(value="/virement",method=RequestMethod.PUT)
+	public boolean virement(
+			@RequestParam String compte1, 
+			@RequestParam String compte2, 
+			@RequestParam double montant, 
+			@RequestParam Long codeEmploye) {
+		return operationsMetier.virement(compte1, compte2, montant, codeEmploye);
+	}
+	
+	
+	
+}
